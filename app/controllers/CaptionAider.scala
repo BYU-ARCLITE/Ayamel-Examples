d04f333d17712a3c88c905f9e8679efca94e4e92
Josh Monson <camman3d@gmail.com>
2013-06-12 15:54:23 -0600

    Finished integrating caption aider.

--- app/controllers/CaptionAider.scala
+++ app/controllers/CaptionAider.scala
@@ -1,10 +1,15 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.Controller
 import controllers.authentication.Authentication
 import play.api.Play
-import models.Content
+import models.{Course, Content}
 import play.api.Play.current
+import service.{AdditionalDocumentAdder, FileUploader, ResourceHelper}
+import java.io.ByteArrayInputStream
+import dataAccess.ResourceController
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,11 +19,76 @@ import play.api.Play.current
  * To change this template use File | Settings | File Templates.
  */
 object CaptionAider extends Controller {
-  def view = Authentication.authenticatedAction() {
+  def view(id: Long, courseId: Long) = Authentication.authenticatedAction() {
     implicit request =>
       implicit user =>
-        val resourceLibraryUrl = Play.configuration.getString("resourceLibrary.baseUrl").get
-        val content = Content.findById(38).get
-        Ok(views.html.captionAider.view(content, resourceLibraryUrl))
+        ContentController.getContent(id) {
+          content =>
+            val course = Course.findById(courseId)
+            val resourceLibraryUrl = Play.configuration.getString("resourceLibrary.baseUrl").get
+            Ok(views.html.captionAider.view(content, course, resourceLibraryUrl))
+        }
   }
+
+  def save = Authentication.authenticatedAction(parse.multipartFormData) {
+    implicit request =>
+      implicit user =>
+
+        val params = request.body.dataParts.mapValues(_(0))
+        val contentId = params("contentId").toLong
+        ContentController.getContent(contentId) {
+          content =>
+
+            val params = request.body.dataParts.mapValues(_(0))
+            val mime = params("mime")
+            val name = params("name")
+            val label = params("label")
+            val language = params("language")
+            val kind = params("kind")
+            val data = params("data")
+            val stream = new ByteArrayInputStream(data.getBytes)
+
+            // We need to determine if this file has already been saved
+            val resourceId = params("resourceId")
+            Async {
+              if (resourceId.isEmpty) {
+                // Create a new resource
+                // Upload the data
+                FileUploader.uploadStream(stream, FileUploader.uniqueFilename(name), data.getBytes.length, mime).flatMap {
+                  url =>
+
+                  // Create subtitle (subject) resource
+                    ResourceHelper.createResourceWithUri(label, "", kind, Nil, "text", url, mime, language).flatMap {
+                      resource =>
+                        val subjectId = (resource \ "id").as[String]
+
+                        AdditionalDocumentAdder.add(content, subjectId, 'captionTrack) {
+                          course =>
+                            Ok
+                        }
+                    }
+                }
+
+              } else {
+                // Figure out which file we are replacing
+                // First get the resource
+                ResourceController.getResource(resourceId).flatMap {
+                  json =>
+                    val resource = json \ "resource"
+
+                    // Now find the file
+                    val url = ((resource \ "content" \ "files")(0) \ "downloadUri").as[String]
+                    val filename = url.substring(url.lastIndexOf("/") + 1)
+
+                    // Replace the file
+                    FileUploader.uploadStream(stream, filename, data.getBytes.length, mime).map {
+                      url =>
+                        Ok
+                    }
+                }
+              }
+            }
+        }
+  }
+
 }
--- public/javascripts/captionAider.js
+++ public/javascripts/captionAider.js
@@ -1,106 +1,306 @@
-function renderCue(cue) {
-    return captionEditor.make(cue);
-}
-
-// Render the content
-
-content.settings = {
-    level: 2,
-    enabledCaptionTracks: content.settings.enabledCaptionTracks,
-    includeTranscriptions: "true"
-};
-contentHolder = $("#contentHolder")[0];
-ContentRenderer.render({
-    content: content,
-    userId: 1,
-    owner: true,
-    teacher: false,
-    courseId: 0,
-    holder: contentHolder,
-    coursePrefix: "",
-    annotate: true,
-    startTime: 0,
-    endTime: -1,
-    renderCue: renderCue,
-    callback: function(args) {
-        var commandStack = new EditorWidgets.CommandStack(),
-            timeline = new Timeline(document.getElementById("timeline"), {
-                stack: commandStack,
-                width: document.body.clientWidth || window.innerWidth,
-                length: 3600,
-                start: 0,
-                end: 240,
-                multi: true,
-                tool: Timeline.SELECT
-            }),
-            timestamp = document.getElementById("timestamp"),
-            automove = document.getElementById("automove"),
-            videoPlayer = args.videoPlayer,
-            renderer = videoPlayer.captionRenderer;
-
-        captionEditor = CaptionEditor({
-            stack: commandStack,
-            renderer: renderer,
-            timeline: timeline
-        });
-
-        window.addEventListener('resize',function(){
-            "use strict";
-            timeline.width = window.innerWidth;
-//            editor.classList[(window.innerHeight < editor.clientHeight + stage.clientHeight)?'add':'remove']("fade");
-        }, false);
-
-        // Set up listeners
-        function setlen(){ timeline.length = videoPlayer.duration; timestamp.textContent = timeline.timeCode; }
-        function frame_change() { timeline.currentTime = this.currentTime; }
-
-        videoPlayer.addEventListener('loadedmetadata',setlen,false);
-        videoPlayer.addEventListener('durationchange',setlen,false);
-        videoPlayer.addEventListener("timeupdate",frame_change.bind(videoPlayer),false);
-        timeline.on('jump', function(time){ videoPlayer.currentTime = time; });
-        timeline.on('move', function(){ renderer.rebuildCaptions(false); });
-        timeline.on('resizer', function(){ renderer.rebuildCaptions(false); });
-        timeline.on('resizel', function(){ renderer.rebuildCaptions(false); });
-
-        timeline.on('delete',function(seg) {
-            "use strict";
-            if(timeline.spanInView(seg.startTime,seg.endTime)){
-                renderer.rebuildCaptions(true);
-            }
-        });
-        timeline.on('create',function(seg) {
-            "use strict";
-//            if(automove.classList.contains('active')){
-//                timeline.currentTool = Timeline.MOVE;
-//                $("#movb").button("toggle");
-//            }
-            if(seg.active){ renderer.rebuildCaptions(false); }
-        });
-        timeline.on('unpaste',function(segs) {
-            "use strict";
-            if(segs.some(function(seg){ return timeline.spanInView(seg.startTime,seg.endTime); })){
-                renderer.rebuildCaptions(true);
-            }
-        });
-        timeline.on('paste',function(segs) {
-            "use strict";
-            if(segs.some(function(seg){ return seg.active; })){ renderer.rebuildCaptions(false); }
-        });
-        timeline.on('merge',function(seg) {
-            "use strict";
-            renderer.rebuildCaptions(true);
-        });
-        timeline.on('unmerge',function(seg) {
-            "use strict";
-            renderer.rebuildCaptions(true);
-        });
-        timeline.on('split',function(seg) {
-            "use strict";
-            renderer.rebuildCaptions(true);
-        });
-        timeline.on('timeupdate', function(){ timestamp.textContent = timeline.timeCode; });
-
-        // For now, load all the available tracks
-
+$(function() {
+    function renderCue(cue) {
+        return captionEditor.make(cue);
     }
+
+    // Render the content
+
+    content.settings = {
+        level: 2,
+        enabledCaptionTracks: content.settings.enabledCaptionTracks,
+        includeTranscriptions: "true"
+    };
+    contentHolder = $("#contentHolder")[0];
+    ContentRenderer.render({
+        content: content,
+        userId: 1,
+        owner: true,
+        teacher: false,
+        courseId: 0,
+        holder: contentHolder,
+        coursePrefix: "",
+        annotate: true,
+        startTime: 0,
+        endTime: -1,
+        renderCue: renderCue,
+        callback: function(args) {
+            var format = "text/vtt",
+                commandStack = new EditorWidgets.CommandStack(),
+                timeline = new Timeline(document.getElementById("timeline"), {
+                    stack: commandStack,
+                    width: document.body.clientWidth || window.innerWidth,
+                    length: 3600,
+                    start: 0,
+                    end: 240,
+                    multi: true,
+                    tool: Timeline.SELECT
+                }),
+                timestamp = document.getElementById("timestamp"),
+                automove = document.getElementById("moveAfterAddButton"),
+                clearRepeatButton = document.getElementById("clearRepeatButton"),
+                enableRepeatButton = document.getElementById("enableRepeatButton"),
+                repeatIcon = document.getElementById("repeatIcon"),
+                videoPlayer = args.videoPlayer,
+                renderer = videoPlayer.captionRenderer;
+
+            captionEditor = CaptionEditor({
+                stack: commandStack,
+                renderer: renderer,
+                timeline: timeline
+            });
+
+            window.addEventListener('resize',function(){
+                "use strict";
+                timeline.width = window.innerWidth;
+    //            editor.classList[(window.innerHeight < editor.clientHeight + stage.clientHeight)?'add':'remove']("fade");
+            }, false);
+
+            // Set up listeners
+            function setlen(){ timeline.length = videoPlayer.duration; timestamp.textContent = timeline.timeCode; }
+            function frame_change() { timeline.currentTime = this.currentTime; }
+
+            videoPlayer.addEventListener('loadedmetadata',setlen,false);
+            videoPlayer.addEventListener('durationchange',setlen,false);
+            videoPlayer.addEventListener("timeupdate",frame_change.bind(videoPlayer),false);
+            timeline.on('jump', function(time){ videoPlayer.currentTime = time; });
+            timeline.on('move', function(){ renderer.rebuildCaptions(false); });
+            timeline.on('resizer', function(){ renderer.rebuildCaptions(false); });
+            timeline.on('resizel', function(){ renderer.rebuildCaptions(false); });
+
+            automove.addEventListener("click", function() {
+                if(automove.classList.contains('active')){
+                    automove.classList.remove("btn-yellow");
+                    automove.classList.add("btn-inverse");
+                } else {
+                    automove.classList.add("btn-yellow");
+                    automove.classList.remove("btn-inverse");
+                }
+            }, false);
+
+            timeline.on('delete',function(seg) {
+                "use strict";
+                if(timeline.spanInView(seg.startTime,seg.endTime)){
+                    renderer.rebuildCaptions(true);
+                }
+            });
+            timeline.on('create',function(seg) {
+                "use strict";
+                if(automove.classList.contains('active')){
+                    timeline.currentTool = Timeline.MOVE;
+                    $("#moveToolButton").button("toggle");
+                }
+                if(seg.active){ renderer.rebuildCaptions(false); }
+            });
+            timeline.on('unpaste',function(segs) {
+                "use strict";
+                if(segs.some(function(seg){ return timeline.spanInView(seg.startTime,seg.endTime); })){
+                    renderer.rebuildCaptions(true);
+                }
+            });
+            timeline.on('paste',function(segs) {
+                "use strict";
+                if(segs.some(function(seg){ return seg.active; })){ renderer.rebuildCaptions(false); }
+            });
+            timeline.on('merge',function(seg) {
+                "use strict";
+                renderer.rebuildCaptions(true);
+            });
+            timeline.on('unmerge',function(seg) {
+                "use strict";
+                renderer.rebuildCaptions(true);
+            });
+            timeline.on('split',function(seg) {
+                "use strict";
+                renderer.rebuildCaptions(true);
+            });
+            timeline.on('timeupdate', function(){ timestamp.textContent = timeline.timeCode; });
+
+            // Track selection
+            videoPlayer.addEventListener("enabletrack", function(event) {
+                timeline.addTextTrack(event.track);
+            });
+            videoPlayer.addEventListener("disabletrack", function(event) {
+                timeline.removeTextTrack(event.track.label);
+            });
+
+            //Bind the toolbar buttons
+
+            // Undo/redo buttons
+            document.getElementById("undoButton").addEventListener('click',function(){ timeline.cstack.undo(); },false);
+            document.getElementById("redoButton").addEventListener('click',function(){ timeline.cstack.redo(); },false);
+
+            // Font size buttons
+            document.getElementById('plusFontButton').addEventListener('click',function(){ renderer.fontSizeRatio += 0.005; },false);
+            document.getElementById('minusFontButton').addEventListener('click',function(){ renderer.fontSizeRatio -= 0.005; },false);
+
+            // Tool buttons
+            function setTool(tool){	timeline.currentTool = tool; }
+            document.getElementById("selectToolButton").addEventListener('click',setTool.bind(null,Timeline.SELECT),false);
+            document.getElementById("moveToolButton").addEventListener('click',setTool.bind(null,Timeline.MOVE),false);
+            document.getElementById("timeShiftToolButton").addEventListener('click',setTool.bind(null,Timeline.SHIFT),false);
+            document.getElementById("addCueToolButton").addEventListener('click',setTool.bind(null,Timeline.CREATE),false);
+            document.getElementById("splitToolButton").addEventListener('click',setTool.bind(null,Timeline.SPLIT),false);
+            document.getElementById("deleteToolButton").addEventListener('click',setTool.bind(null,Timeline.DELETE),false);
+            document.getElementById("scrollToolButton").addEventListener('click',setTool.bind(null,Timeline.SCROLL),false);
+            document.getElementById("reorderToolButton").addEventListener('click',setTool.bind(null,Timeline.ORDER),false);
+            document.getElementById("repeatToolButton").addEventListener('click',setTool.bind(null,Timeline.REPEAT),false);
+
+            // AB Repeat Controls
+            clearRepeatButton.addEventListener('click',function(){ timeline.clearRepeat(); },false);
+            enableRepeatButton.addEventListener('click',function(){ timeline.abRepeatOn = !timeline.abRepeatOn; },false);
+
+            timeline.on('abRepeatEnabled',function(){
+                enableRepeatButton.title = "Disable Repeat";
+                repeatIcon.className = "icon-circle";
+            });
+            timeline.on('abRepeatDisabled',function(){
+                enableRepeatButton.title = "Enable Repeat";
+                repeatIcon.className = "icon-circle-blank";
+            });
+            timeline.on('abRepeatSet',function(){
+                [clearRepeatButton, enableRepeatButton].forEach(function(b){ b.classList.remove('disabled'); });
+            });
+            timeline.on('abRepeatUnset',function(){
+                [clearRepeatButton, enableRepeatButton].forEach(function(b){ b.classList.add('disabled'); });
+            });
+
+            // Track buttons
+            document.getElementById("createTrackButton").addEventListener("click", function() {
+                var $trackName = $("#trackName"),
+                    type = $("#trackType").val(),
+                    name = $trackName.val() || "Untitled",
+                    language = $("#trackLanguage").val(),
+                    track = new TextTrack(type, name, language);
+                track.mode = "showing";
+                track.readyState = TextTrack.LOADED;
+                timeline.addTextTrack(track, true);
+                $('#newTrackModal').modal('hide');
+
+                // Add the track to the player
+                videoPlayer.captionRenderer.addTextTrack(track);
+                videoPlayer.controlBar.addTrack(track);
+
+                // Clear the form
+                $trackName.val("");
+            });
+
+            // Format dropdown menu
+            (function(){
+                "use strict";
+                var $li, li, key, formats = TimedText.mime_types,
+                    formatMenu = document.getElementById('formatMenu'),
+                    formatLabel = document.getElementById('formatLabel');
+                for(key in formats){
+                    $li = $("<li><a href='#'></a></li>");
+                    li = $li[0];
+                    $li.children("a").html(formats[key].extension.toUpperCase());
+                    li['data-key'] = key;
+                    li.addEventListener('click',function(){
+                        format = this['data-key'];
+                        formatLabel.innerHTML = $(this).children("a").text();
+                    },false);
+                    formatMenu.appendChild(li);
+                }
+            }());
+
+            // Add save destinations
+            (function () {
+                var targets = EditorWidgets.Save.targets, key;
+                for(key in targets) if(targets.hasOwnProperty(key)){
+                    $("#saveDestinations").append('<label class="radio">' +
+                        '<input type="radio" name="saveDestination" value="' + key + '">' + targets[key].label.replace("To ", "") + '</label>');
+                }
+            }());
+
+            // Saving modal opening
+            $("#saveTrackModal").on("show", function () {
+                // Populate the track select
+                var $tracksToSave = $("#tracksToSave").html("");
+                Object.keys(timeline.trackIndices).map(function(name){
+                    $tracksToSave.append('<option value="' + name + '">' + name + '</option>');
+                });
+            });
+
+            // The saving mechanism
+            $("#saveTrackButton").click(function() {
+                var tracks = $("#tracksToSave").val(),
+                    destination = $("input[name=saveDestination]:checked").val(),
+                    key, data, textTrack;
+                if (tracks && tracks.length) {
+                    var exportedTracks = timeline.exportTracks(format, tracks);
+                    if (destination === "ayamel") {
+
+                        // Saving to the server. Provide all the information and data and let it handle everything
+                        for (key in exportedTracks) {
+                            textTrack = timeline.getTrack(tracks[key]).textTrack;
+                            data = new FormData();
+                            data.append("mime", exportedTracks[key].mime);
+                            data.append("name", exportedTracks[key].name);
+                            data.append("label", textTrack.label);
+                            data.append("data", exportedTracks[key].data);
+                            data.append("language", textTrack.language);
+                            data.append("kind", textTrack.kind);
+                            data.append("resourceId", textTrack.resourceId || "");
+                            data.append("contentId", content.id);
+                            $.ajax({
+                                url: "/captionaider/save",
+                                data: data,
+                                cache: false,
+                                contentType: false,
+                                processData: false,
+                                type: "post",
+                                success: function (data) {
+                                    alert("Saved Successfully");
+                                }
+                            })
+                        }
+                    } else {
+
+                        // Use one of the editor widget saving mechanisms
+                        EditorWidgets.Save(
+                            exportedTracks, destination,
+                            function(){
+                                commandStack.saved = true;
+                                alert("Saved Successfully");
+                            },
+                            function(){ alert("Error Saving; please try again."); }
+                        );
+                    }
+                }
+                $("#saveTrackModal").modal("hide");
+            });
+
+            // Add load sources
+            (function () {
+                var sources = EditorWidgets.LocalFile.sources;
+                for(key in sources) if(sources.hasOwnProperty(key)){
+                    $("#loadDestinations").append('<label class="radio">' +
+                        '<input type="radio" name="loadDestination" value="' + key + '">' + sources[key].label + '</label>');
+                }
+            }());
+
+            // Loading a track
+            $("#loadTrackButton").click(function() {
+                var kind = $("#loadType").val();
+                var language = $("#loadLanguage").val();
+                var where = $("#loadDestination").val();
+                EditorWidgets.LocalFile(where,/.*\.(vtt|srt)/,function(fileObj){
+                    TextTrack.parse({
+                        content: fileObj.data,
+                        mime: fileObj.mime,
+                        kind: kind,
+                        label: fileObj.name,
+                        lang: language,
+                        success: function() {
+                            this.mode = "showing";
+                            timeline.addTextTrack(this,true);
+                            videoPlayer.captionRenderer.addTextTrack(this);
+                            videoPlayer.controlBar.addTrack(this);
+                        }
+                    });
+                });
+                $("#loadTrackModal").modal("hide");
+            });
+        }
+    });
 });
