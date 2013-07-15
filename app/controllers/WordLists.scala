235be4c3fa58dbcd2563db20009af73574c45054
Josh <camman3d@gmail.com>
2013-07-15 11:56:05 -0600

    More word lists. You can now export to Quizlet

--- app/controllers/WordLists.scala
+++ app/controllers/WordLists.scala
@@ -1,7 +1,12 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 import controllers.authentication.Authentication
+import service.HashTools
+import dataAccess.Quizlet
+import play.api.Logger
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,5 +31,49 @@ object WordLists extends Controller {
         Ok(views.html.words.view(wordList))
   }
 
-  def export = TODO
+  def export = Authentication.authenticatedAction() {
+    implicit request =>
+      implicit user =>
+        val data = Map(
+          "response_type" -> Seq("code"),
+          "client_id" -> Seq(Quizlet.clientId),
+          "scope" -> Seq("write_set"),
+          "state" -> Seq(HashTools.md5Hex(request.session.toString)),
+          "redirect_uri" -> Seq(routes.WordLists.authorize().absoluteURL() + "?lang=" + request.queryString("language")(0))
+        )
+        Redirect("https://quizlet.com/authorize/", data)
+  }
+
+  def authorize = Authentication.authenticatedAction() {
+    implicit request =>
+      user =>
+        // Check for an error
+        if (request.queryString.contains("error")) {
+          Ok(request.queryString("error")(0))
+        } else {
+
+          // Check the state
+//          val state = HashTools.md5Hex(request.session.toString)
+
+          // Get the auth token
+          val code = request.queryString("code")(0)
+          Async {
+            Quizlet.getAuthToken(code).flatMap(token => {
+
+              // Export the list
+              // TODO: Get the two letter language code
+              // TODO: Get the language name
+              // TODO: Get the definitions
+              val language = request.queryString("lang")(0)
+              val langName = language
+              val wordList = user.getWordList.filter(_.language == language).map(_.word).distinct.map(word => (word, word))
+
+              Quizlet.createSet(token, s"$langName words from Ayamel", wordList, language.substring(0,2)).map(url => {
+                Redirect(routes.WordLists.view()).flashing("success" -> ("Word list created. <a href=\"" + url + "\">View it now</a>."))
+              })
+            })
+          }
+        }
+
+  }
 }
