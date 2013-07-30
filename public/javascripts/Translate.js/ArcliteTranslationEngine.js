ff494df848e19db0934b0b81059d31ba3748e62d
Josh <camman3d@gmail.com>
2013-07-29 18:23:48 -0600

    Updated the ARCLITE translation to go from the new translate.byu.edu domain.

--- public/javascripts/Translate.js/ArcliteTranslationEngine.js
+++ public/javascripts/Translate.js/ArcliteTranslationEngine.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var arcliteService = function arcliteService(text, srcLang, destLang, callback, error) {
-        var url = "http://sartre3.byu.edu:9009/api/v1/lookup?srcLang=" + srcLang + "&destLang=" + destLang + "&word=" + encodeURIComponent(text);
+        var url = "http://translate.byu.edu/api/v1/lookup?srcLang=" + srcLang + "&destLang=" + destLang + "&word=" + encodeURIComponent(text);
         $.ajax(url, {
             dataType: "json",
             success: function (data) {
