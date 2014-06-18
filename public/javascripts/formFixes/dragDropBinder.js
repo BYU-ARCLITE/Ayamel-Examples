f1eaa5d650b1bb37b627330bc81381f99eb2c6f2
Logan Kearsley <chronosurfer@gmail.com>
2014-06-18 16:10:47 -0600

    Fix drag-n'-drop file uploads.
    This really should be ractivized.

--- public/javascripts/formFixes/dragDropBinder.js
+++ public/javascripts/formFixes/dragDropBinder.js
@@ -1,23 +1,18 @@
 /**
  * For usage, see https://github.com/BYU-ARCLITE/Ayamel-Examples/wiki/Improving-forms
  * @param element
- * @param fileInput
  * @param dropCallback
  * @param enterCallback
  */
-function bindDropArea(element, fileInput, dropCallback, enterCallback) {
-    element.addEventListener("dragenter", function(e) {
-        if (enterCallback)
-            enterCallback();
-    });
+function bindDropArea(element, dropCallback, enterCallback) {
+    element.addEventListener("dragenter", enterCallback);
     element.addEventListener("dragover", function(e) {
         e.preventDefault();
     });
     element.addEventListener("drop", function(e) {
         e.preventDefault();
         e.stopPropagation();
-        fileInput.files = e.dataTransfer.files;
-        if (dropCallback) {
+        if (typeof dropCallback === 'function') {
             dropCallback(e.dataTransfer.files);
         }
     });
