18ac379afdd2d464dbd5b0038c6f15102ea9e3df
Santiago Verdu <verdunator@gmail.com>
2014-10-08 17:04:45 -0600

    Sort Content by Title

--- public/javascripts/contentSelection/ContentItemRenderer.js
+++ public/javascripts/contentSelection/ContentItemRenderer.js
@@ -167,7 +167,9 @@ var ContentItemRenderer = (function() {
             '<div class="btn-group" data-toggle="buttons-radio">\
                 <button class="btn" data-organization="contentType"><i class="icon-play-circle"></i> Content Type</button>\
                 <button class="btn" data-organization="labels"><i class="icon-tags"></i> Labels</button>\
+                <button class="btn" data-organization="title"><i class="icon-sort-by-alphabet"></i> Title</button>\
             </div>'
+            // <button class="btn" data-organization="language"><i class="icon-globe"></i> Language</button>\
         );
 
         $(element).button();
@@ -242,24 +244,44 @@ var ContentItemRenderer = (function() {
                     content: args.content,
                     holder: args.holder,
                     format: args.format,
-					organization: args.organization,
+                    organization: args.organization,
                     sorting: args.sorting,
                     labels: args.labels,
                     filters: args.filters,
                     courseId: args.courseId,
                     click: args.click
                 }));
-
                 if (args.organization === "labels") {
                     filters = {};
                     args.labels.forEach(function (label) {
-                        filters['<h3><i class="icon-tag"></i> ' + label + '</h3>'] = function (content) {
+                        filters['<h3><i class="icon-tag"></i> ' + label + '</h3>'] = function (content) { // where the freak does this come from
                             return content.labels.indexOf(label) >= 0;
                         };
+
                     });
                     filters['<h3>Unlabeled</h3>'] = function (content) {
                         return content.labels.length === 0;
                     };
+                } else if (args.organization === "language") {
+                    filters = {};
+                    // for each content object, get the language from the resource library and add it to the filter
+                    args.content.forEach(function(content){
+                        ResourceLibrary.load(content.resourceId).then(function(resource){
+                            var lang = resource.languages.iso639_3.map(function(langCode) {
+                                return '<em class="pad-left-high">' + Ayamel.utils.getLangName(langCode) + '</em>';
+                            }).join('');
+                            filters['<h3><i class="icon-globe"></i> ' + lang + '</h3>'] = true;             
+                        });
+                    });
+
+                } else if (args.organization === "title") {
+                    filters = {};
+                    args.content.forEach(function (obj) {
+                        filters['<h3> ' + obj.name[0] + '</h3>'] = function (content) {
+                            // filters the content by the first letter.
+                            return (content.name[0] === obj.name[0]) ? true : false;
+                        };
+                    });
                 }
             } else {
                 args.organization = "contentType";
@@ -271,22 +293,27 @@ var ContentItemRenderer = (function() {
                 console.log("TODO: Setup sorting");
             }
 
-
             // Add the content to the holder
             if (filters) {
 
                 // Filter the content into categories
-                Object.keys(filters).forEach(function(filterName){
-
+                // Also sorts the map so the filters will be sorted alphabetically
+                Object.keys(filters).sort().forEach(function(filterName){
                     // Filter the content
                     var filteredContent = args.content.filter(filters[filterName]);
+
+                    // Sort the content inside of the filter
+                    filteredContent.sort(function(a, b) {
+                        a = a.name;
+                        b = b.name;
+                        return b > a ? -1 : b < a ? 1 : 0;
+                    });
 
                     // If there were results then show them
                     if (filteredContent.length) {
 
                         // Add the name of the filter
                         args.holder.appendChild(Ayamel.utils.parseHTML(filterName));
-
                         // Add the content
                         var contentHolder = document.createElement('div');
                         contentHolder.className = "contentHolder " + args.format + "Format";
