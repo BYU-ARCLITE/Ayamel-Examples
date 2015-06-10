8b42f3503afaf7b50b87f9feab842a9c887b082d
johnlarson <john.robert.larson@gmail.com>
2015-06-10 16:50:25 -0600

    got OpenID working

--- app/controllers/authentication/Google.scala
+++ app/controllers/authentication/Google.scala
@@ -4,13 +4,18 @@ import play.api.mvc.{Action, Controller}
 import java.security.SecureRandom
 import play.api.libs.ws.{WS, Response}
 import play.api.libs.json.{Json, JsObject}
+import play.api.Logger
 import concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
+import scala.concurrent.Await
 import ExecutionContext.Implicits.global
 
 /**
  * Controller which handles Google authentication.
  */
 object Google extends Controller {
+
+  val logger = Logger("application")
 
   /**
    * Redirects to the Google login page. Uses OpenID
@@ -23,52 +28,90 @@ object Google extends Controller {
       random.nextBytes(byte_array)
       val state_token = byte_array.mkString
 
+      val redirect_route = routes.Google.callback()
+      val redirect_uri = redirect_route.absoluteURL()
+
       //Figure out how to save the action & the state token in the session or something
 
       Redirect("https://accounts.google.com/o/oauth2/auth", Map(
           "client_id" -> Seq("1052219675733-16ul2rbrpm05reqe8cra14ib4m0j8bt8.apps.googleusercontent.com"),
           "response_type" -> Seq("code"),
           "scope" -> Seq("openid email"),
-          "redirect_uri" -> Seq("https://ayamel.byu.edu/auth/google/callback/"),
+          "redirect_uri" -> Seq(redirect_uri),
           "state" -> Seq(state_token)
         ), 303
       )
   }
 
-  def decodeIdToken(jwt: String) = {
-    Json.obj(
-        "test" -> "test"
-    )
+  def decodeIdTokenJson(jwt: String) = {
+    val idTokenJsonString = getIdTokenJsonString(jwt)
+    val result = Json.parse(idTokenJsonString)
+    result
+  }
+
+  // Change this to get id token string internally using discovery document
+  def getIdTokenJsonString(jwt: String): String = {
+    val jsonAddress : String = "https://www.googleapis.com/oauth2/v1/tokeninfo"
+    val jsonAwaitable = WS.url(jsonAddress).post(
+        Map (
+          "id_token" -> Seq(jwt)
+        )
+      ).map {
+      response: Response =>
+        response.body
+    }
+    Await.result(jsonAwaitable, Duration(1000, "millis"))
   }
   
   /**
    * When the Google login is successful, it is redirected here, where user info is extracted and the user is logged in.
    */
-  def callback(action: String, path: String = "") = Action {
+  def callback() = Action {
     implicit request =>
+
+      val action = "login"
+      val path = ""
+
       // Eliminate unchecked ".get"s
       // Confirm anti-forgery state token
-      val state: Seq[String] = request.queryString.get("state").get
+      val stateSeq: Seq[String] = request.queryString.get("state").get
+      val state: String = stateSeq(0)
 
-      val code: Seq[String] = request.queryString.get("code").get
+      val codeSeq: Seq[String] = request.queryString.get("code").get
+      val code: String = codeSeq(0)
+
+      val redirect_route = routes.Google.callback()
+      val redirect_uri = redirect_route.absoluteURL()
+
       Async {
-        WS.url("Place Holder for the token endpoint ").post(
+        WS.url(getTokenEndPoint).post(
           Map(
-            "code" -> code,
+            "code" -> codeSeq,
             "client_id" -> Seq("1052219675733-16ul2rbrpm05reqe8cra14ib4m0j8bt8.apps.googleusercontent.com"),
             "client_secret" -> Seq("YcoCdjWna_-EFvoTxwx_q2uK"),
-            "redirect_uri" -> Seq("https://ayamel.byu.edu/auth/google/callback/"),
+            "redirect_uri" -> Seq(redirect_uri),
             "grant_type" -> Seq("authorization_code")
           )
         ).map {
           response: Response =>
             try {
+
               val json = response.json
-              val id_token = (json \ "id_token").as[String]
-              val id_json = decodeIdToken(id_token)
+              val id_token_property = (json \ "id_token")
+              val id_token = id_token_property.as[String]
+              val id_json = decodeIdTokenJson(id_token)
               
               // check that the email was actually verified - there's a boolean property of the returned JSON object for that
-              val email = (id_json \ "email").as[String]
+              val email_verified_property = id_json \ "email_verified"
+              val email_verified = email_verified_property.toString
+              
+              val email_property = (id_json \ "email")
+              val email_class = email_property.getClass
+              val email_class_name = email_class.getName
+              val email = email_property.toString
+
+              logger.debug(email)
+
               val user = Authentication.getAuthenticatedUser(email, 'google, None, Some(email))
 
               //Need to figure out how to get the action from the login method
@@ -83,5 +126,8 @@ object Google extends Controller {
         }
       }
   }
+
+  // Placeholder. Should get it from the discovery document
+  def getTokenEndPoint() = "https://www.googleapis.com/oauth2/v3/token"
 
 }
