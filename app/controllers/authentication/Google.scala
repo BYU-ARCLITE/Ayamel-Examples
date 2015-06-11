cbab051914c5f2ec011954e673c4bed50b586648
Logan Kearsley <chronosurfer@gmail.com>
2015-06-10 18:11:03 -0600

    Dynamically load the discovery document.
    TODO: Cache it!

--- app/controllers/authentication/Google.scala
+++ app/controllers/authentication/Google.scala
@@ -1,5 +1,6 @@
 package controllers.authentication
 
+import org.apache.commons.codec.binary.Base64
 import play.api.mvc.{Action, Controller}
 import play.api.mvc.Results.InternalServerError
 import java.security.SecureRandom
@@ -29,17 +30,16 @@ object Google extends Controller {
       random.nextBytes(byte_array)
       val state_token = byte_array.mkString
 
-      val redirect_route = routes.Google.callback()
-      val redirect_uri = redirect_route.absoluteURL()
+      val redirect_uri = routes.Google.callback().absoluteURL()
 
       //Figure out how to save the action & the state token in the session or something
 
-      Async {
-        WS.url("https://accounts.google.com/.well-known/openid-configuration")
+      try {
+        Async {
+          WS.url("https://accounts.google.com/.well-known/openid-configuration")
           .get().map { discovery: Response =>
-          try {
             val auth_endpoint = (discovery.json \ "authorization_endpoint")
-            Redirect(auth_endpoint, Map(
+            Redirect(auth_endpoint.as[String], Map(
                 "client_id" -> Seq("1052219675733-16ul2rbrpm05reqe8cra14ib4m0j8bt8.apps.googleusercontent.com"),
                 "response_type" -> Seq("code"),
                 "scope" -> Seq("openid email"),
@@ -47,30 +47,19 @@ object Google extends Controller {
                 "state" -> Seq(state_token)
               ), 303
             )
-          } catch(_)
-            InternalServerError
+          }
         }
+      } catch {
+        case _: Exception =>
+          InternalServerError
       }
   }
 
   def decodeIdTokenJson(jwt: String) = {
-    val idTokenJsonString = getIdTokenJsonString(jwt)
-    val result = Json.parse(idTokenJsonString)
-    result
-  }
-
-  // Change this to get id token string internally using discovery document
-  def getIdTokenJsonString(jwt: String): String = {
-    val jsonAddress : String = "https://www.googleapis.com/oauth2/v1/tokeninfo"
-    val jsonAwaitable = WS.url(jsonAddress).post(
-        Map (
-          "id_token" -> Seq(jwt)
-        )
-      ).map {
-      response: Response =>
-        response.body
-    }
-    Await.result(jsonAwaitable, Duration(1000, "millis"))
+    val b64payload = jwt.split('.')(1)
+    val jsString = Base64.decodeBase64(b64payload.getBytes("utf-8")).mkString
+    logger.debug(jsString)
+    Json.parse(jsString)
   }
   
   /**
@@ -85,59 +74,36 @@ object Google extends Controller {
       // Eliminate unchecked ".get"s
       // Confirm anti-forgery state token
       val stateSeq: Seq[String] = request.queryString.get("state").get
-      val state: String = stateSeq(0)
-
       val codeSeq: Seq[String] = request.queryString.get("code").get
-      val code: String = codeSeq(0)
-
-      val redirect_route = routes.Google.callback()
-      val redirect_uri = redirect_route.absoluteURL()
+      val redirect_uri = routes.Google.callback().absoluteURL()
 
       Async {
-        WS.url(getTokenEndPoint).post(
-          Map(
-            "code" -> codeSeq,
-            "client_id" -> Seq("1052219675733-16ul2rbrpm05reqe8cra14ib4m0j8bt8.apps.googleusercontent.com"),
-            "client_secret" -> Seq("YcoCdjWna_-EFvoTxwx_q2uK"),
-            "redirect_uri" -> Seq(redirect_uri),
-            "grant_type" -> Seq("authorization_code")
+        WS.url("https://accounts.google.com/.well-known/openid-configuration").get()
+        .flatMap { discovery: Response =>
+          WS.url((discovery.json \ "token_endpoint").as[String]).post(
+            Map(
+              "code" -> codeSeq,
+              "client_id" -> Seq("1052219675733-16ul2rbrpm05reqe8cra14ib4m0j8bt8.apps.googleusercontent.com"),
+              "client_secret" -> Seq("YcoCdjWna_-EFvoTxwx_q2uK"),
+              "redirect_uri" -> Seq(redirect_uri),
+              "grant_type" -> Seq("authorization_code")
+            )
           )
-        ).map {
-          response: Response =>
-            try {
-
-              val json = response.json
-              val id_token_property = (json \ "id_token")
-              val id_token = id_token_property.as[String]
-              val id_json = decodeIdTokenJson(id_token)
+        }.map { response: Response =>
+          logger.debug(response.body)
+          val id_token = (response.json \ "id_token").as[String]
+          val id_json = decodeIdTokenJson(id_token)
               
-              // check that the email was actually verified - there's a boolean property of the returned JSON object for that
-              val email_verified_property = id_json \ "email_verified"
-              val email_verified = email_verified_property.toString
-              
-              val email_property = (id_json \ "email")
-              val email_class = email_property.getClass
-              val email_class_name = email_class.getName
-              val email = email_property.toString
+          // check that the email was actually verified - there's a boolean property of the returned JSON object for that
+          val email_verified = (id_json \ "email_verified").as[String]
+          val email = (id_json \ "email").as[String]
 
-              logger.debug(email)
+          logger.debug(email)
 
-              val user = Authentication.getAuthenticatedUser(email, 'google, None, Some(email))
+          val user = Authentication.getAuthenticatedUser(email, 'google, None, Some(email))
 
-              //Need to figure out how to get the action from the login method
-              //if (action == "merge")
-              //  Authentication.merge(user)
-              //else
-                Authentication.login(user, path)
-            } catch {
-              case _: Exception =>
-                Redirect("https://ayamel.byu.edu/")
-            }
+          Authentication.login(user, path)
         }
       }
   }
-
-  // Placeholder. Should get it from the discovery document
-  def getTokenEndPoint() = "https://www.googleapis.com/oauth2/v3/token"
-
 }
