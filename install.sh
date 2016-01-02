428fba00817f9e5a6f4bb068552374817309ea04
Logan Kearsley <chronosurfer@gmail.com>
2016-01-02 13:26:18 -0700

    Remove useless scripts.

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd public
-git clone https://github.com/BYU-ARCLITE/subtitle-timeline-editor.git
-git clone https://github.com/BYU-ARCLITE/EditorWidgets.git
-git clone https://github.com/BYU-ARCLITE/TimedText
-git clone https://github.com/BYU-ARCLITE/Ayamel.js.git
