10d27618bd9fdbed13b78ecd0ca33a37efb8ec35
Logan Kearsley <chronosurfer@gmail.com>
2016-02-23 18:17:25 -0700

    Add support for SCOLA authentication
    Need to move configurable values into configuration file.

---
+++ app/controllers/Scola.scala
@@ -0,0 +1,69 @@
+package controllers
+
+import authentication.Authentication
+import play.api.mvc.Controller
+import play.api.Play.{current, configuration}
+import play.api.Logger
+import java.net.URL
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import javax.xml.bind.DatatypeConverter
+import scala.concurrent.{Future, ExecutionContext}
+import ExecutionContext.Implicits.global
+
+/**
+ * Controller for generating signed SCOLA URLs.
+ */
+object Scola extends Controller {
+
+  // These values are configurable and should be pulled from config.
+  val lifespan = configuration.getLong("scola.lifetime").getOrElse[Long](28800)
+  val sharedSecret = configuration.getString("scola.secret").getOrElse("SuperSecret")
+  val parameterPrefix = configuration.getString("scola.prefix").getOrElse("scolalop")
+  val algorithm = configuration.getString("scola.algorithm").getOrElse("SHA-256")
+  val haship = configuration.getBoolean("scola.haship").getOrElse(false)
+
+  def buildUrl = Authentication.authenticatedAction(parse.multipartFormData) {
+    implicit request =>
+	  implicit user =>
+        val data = request.body.dataParts
+        val url = new URL(data("url")(0))
+        val path = url.getPath()
+
+        // Set start time to five minutes ago to account for clock shifts, values in seconds
+        val startTime = (System.currentTimeMillis() / 1000) - 300;
+        val endTime = startTime + lifespan;
+
+        val startParam = s"${parameterPrefix}starttime=$startTime"
+        val endParam = s"${parameterPrefix}endtime=$endTime"
+        val parameters = List[String](
+          sharedSecret, startParam, endParam
+        )
+
+        val query = (if (haship) request.remoteAddress :: parameters else parameters)
+                    .sorted
+                    .mkString("&")
+
+        // Strip off the manifest file and front slash from the path for hashing
+	    val nameindex = path.lastIndexOf("/")
+        val subpath = path.substring(1, if(nameindex > -1) nameindex else path.length)
+        val input = s"${subpath}?${query}".getBytes("UTF-8")
+
+        // Use DataTypeConverter instead of Base64 as Base64 library doesn't pad correctly.
+        // But DataTypeConverter doesn't URL escape the strings, so URLify the result.
+        val hash = DatatypeConverter.printBase64Binary(
+                     MessageDigest.getInstance(algorithm).digest(input)
+                   ).replace("+", "-").replace("/", "_");
+
+        // Build non-secret values into url for validation on the otherside, along with the calculated hash.
+        val response = url.getProtocol() + "://" +
+                       url.getAuthority() +
+                       path + "?" +
+                       startParam + "&" +
+                       endParam + "&" +
+                       s"${parameterPrefix}hash=${hash}"
+	  
+	    Logger.debug("SCOLA url: $url\nSCOLA input: $input\nSCOLA hash: $hash")
+        Future(Ok(response))
+  }
+}
--- app/service/ResourceHelper.scala
+++ app/service/ResourceHelper.scala
@@ -34,6 +34,13 @@ object ResourceHelper {
     uri.startsWith("rtmp://") || uri.startsWith("rtmpe://")
 
   /**
+   * Determines if the given URL is a SCOLA url or not
+   * @param uri The URL to check
+   * @return
+   */
+  def isSCOLA(uri: String): Boolean = uri.startsWith("scola://")
+
+  /**
    * Determines if the given URL is to a YouTube video or not
    * @param uri The URL to check
    * @return
@@ -73,7 +80,8 @@ object ResourceHelper {
    * @param uri The URL to check
    * @return
    */
-  def isValidUrl(uri: String): Boolean = isHTTP(uri) || isRTMP(uri) ||
+  def isValidUrl(uri: String): Boolean =
+    isHTTP(uri) || isRTMP(uri) || isSCOLA(uri) ||
     isYouTube(uri) || isBrightcove(uri) || isVimeo(uri) || isOoyala(uri)
 
   /**
