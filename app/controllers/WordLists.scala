d52b6dd0231fc6ecfbe3d643dfabf5e4564ddba6
dro248 <david.ostler001@gmail.com>
2016-05-16 12:30:36 -0600

    Button4, remove normalize

--- app/controllers/WordLists.scala
+++ app/controllers/WordLists.scala
@@ -30,10 +30,10 @@ object WordLists extends Controller {
   /**
    * Adds a word (or text) to a word list. For AJAX calls
    */
-  def add = Authentication.authenticatedAction(parse.multipartFormData) {
+  def add = Authentication.authenticatedAction(parse.multipartFormData) {  //urlFormEncoded
     implicit request =>
       implicit user =>
-        user.addWord(normalize(request.body("word")(0)), request.body("srcLang")(0), request.body("destLang")(0))
+        user.addWord((request.body("word")(0)), request.body("srcLang")(0), request.body("destLang")(0))
         Future(Ok)
   }
 
